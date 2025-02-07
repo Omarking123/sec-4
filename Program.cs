7e41409780ff1f8fc20a082753da71afc596028c
Omar <Omar@DESKTOP-OUKQKJG>
2025-02-07 16:01:46 +0300

    video 2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace sec_4
+﻿using System.Collections;
+
+namespace sec_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region  video 2
+            //Hashtable phonebook = new Hashtable();
+            //phonebook.Add("ahmed", 111111);
+            //phonebook.Add("ali", 22222);
+            //phonebook.Add("amr", 33333);
+            ////phonebook.Add(null, 111111);
+
+            ////Console.WriteLine(phonebook.IsFixedSize);
+            ////Console.WriteLine(phonebook.IsReadOnly);
+
+            //phonebook["ahmessed"] = 123123123;
+            //var flag =  phonebook.Contains("ahmed");
+            //Console.WriteLine(flag);
+
+
+            //foreach (var item in phonebook.Keys)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+            //foreach (var item in phonebook)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            #endregion
         }
     }
 }
